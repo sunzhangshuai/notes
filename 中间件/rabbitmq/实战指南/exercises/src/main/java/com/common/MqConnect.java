49769e78f1fc85d35e282784acfbe1dd3c97fada
sunchen <sunchen@trawe.cn>
2021-06-14 13:56:02 +0800

    rabbit实战代码

---
+++ 中间件/rabbitmq/实战指南/exercises/src/main/java/com/common/MqConnect.java
@@ -0,0 +1,86 @@
+package com.common;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Connect:
+ *
+ * @author sunchen
+ * @date 2021/6/6 11:41 上午
+ */
+public class MqConnect {
+
+    /**
+     * 协议
+     */
+    static String protocol = "amqp";
+
+    /**
+     * 域名
+     */
+    static String hostName = "127.0.0.1";
+
+    /**
+     * 端口
+     */
+    static int port = 5672;
+
+    /**
+     * 用户名
+     */
+    static String username = "guest";
+
+    /**
+     * 密码
+     */
+    static String password = "guest";
+
+    /**
+     * 虚拟主机
+     */
+    static String virtualHost = "/study";
+
+    /**
+     * 常用connection
+     *
+     * @return 连接
+     * @throws IOException      ...
+     * @throws TimeoutException ...
+     */
+    public static Connection connection() throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(hostName);
+        connectionFactory.setPort(port);
+        connectionFactory.setVirtualHost(virtualHost);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory.newConnection();
+    }
+
+    /**
+     * 通过uri的方式连接
+     *
+     * @return 连接
+     * @throws IOException              ...
+     * @throws TimeoutException         ...
+     * @throws NoSuchAlgorithmException ...
+     * @throws KeyManagementException   ...
+     * @throws URISyntaxException       ...
+     */
+    public static Connection connectionByUri() throws IOException, TimeoutException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        String uri = protocol + "://" + username + ":" + password + "@" + hostName + ":" + port + virtualHost;
+        System.out.println(uri);
+        connectionFactory.setUri(uri);
+        return connectionFactory.newConnection();
+    }
+
+
+}
---
+++ 中间件/rabbitmq/实战指南/exercises/src/main/java/com/dxq/Consumer.java
@@ -0,0 +1,45 @@
+package com.dxq;
+
+import com.common.MqConnect;
+import com.common.CommonConsumer;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Producer:
+ *
+ * @author sunchen
+ * @date 2021/6/2 9:10 下午
+ */
+public class Consumer {
+    static String consumerTag = "consumer:study:dxq";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 连接
+        Connection connection = MqConnect.connection();
+        assert connection != null;
+
+        // 信道
+        Channel channel = connection.createChannel();
+
+        // 推模式开始消费
+        channel.basicConsume(Producer.originQueueName, false, consumerTag, new CommonConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body)
+                    throws IOException
+            {
+                System.out.println(Arrays.toString(body));
+                channel.basicReject(envelope.getDeliveryTag(), false);
+            }
+        });
+    }
+}
